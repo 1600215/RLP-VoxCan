b86c1f8e3f274583a8341601c5ae6474be972657
albertceballos0 <albertceballos37@gmail.com>
2024-06-05 17:54:04 +0200

    servidor 100% funcional, falta habilitar https y test del estado standby falta ensamblar con bucle funcional del robot

--- raspi/test/testStandby/testStandby.py
+++ raspi/test/testStandby/testStandby.py
@@ -1,52 +1,129 @@
 import os
+import sys
 import time
-import requests
+import aiohttp
+import asyncio
 import random
+import speech_recognition as sr
+from pydub import AudioSegment
+import io
 
-AUDIO_FOLDER = './uploads'  # Cambia esto por la ruta de tu carpeta de destino
-SERVER = 'http://localhost:3000'
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
+from constants import State
 
-def predict(audio):
-    # Genera un número aleatorio entre 0 y 1
+AUDIO_FOLDER = '../../../webServer/uploads'  # Cambia esto por la ruta de tu carpeta de destino
+SERVER_URL = 'http://localhost:3000'
+
+async def predict(audio):
+    await asyncio.sleep(4)
     r = random.random()
-    if r < 0.8:
+    if r < 0.2:
         return 4
     else:
-        # Retorna 0, 1, 2 o 3 con igual probabilidad (0.05 cada uno)
         return random.choice([0, 1, 2, 3])
 
-print("Esperando comando desde la Raspberry Pi...")
+async def set_command(file_path, person, text):
+    print("Enviando comando...", file_path, person, text)
+    data = {'filename': os.path.basename(file_path), 'person': person, 'text': text}
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f'{SERVER_URL}/set-command', data=data) as response:
+            if response.status == 200:
+                print("Comando enviado correctamente:", await response.text())
+            else:
+                print("Error al enviar el comando:", await response.text())
 
-while True:
-    archivos = os.listdir(AUDIO_FOLDER)
+async def finish_command():
+    async with aiohttp.ClientSession() as session:
+        print("Finalizando comando...")
+        async with session.post(f'{SERVER_URL}/finish-command') as response:
+            if response.status == 200:
+                print("Comando finalizado correctamente")
+            else:
+                print("Error al finalizar el comando:", await response.text())
+
+async def recognize_audio_from_file(file_path):
+    recognizer = sr.Recognizer()
+
+    person = await predict(audio=file_path)
+    if person == 4:
+        return None
     
+    audio = AudioSegment.from_mp3(file_path)
+    wav_data = io.BytesIO()
+    audio.export(wav_data, format="wav")
+
+    with io.BytesIO(wav_data.getvalue()) as wav_io:
+        wav_io.seek(0)
+        with sr.AudioFile(wav_io) as source:
+            audio_data = recognizer.record(source)
+            try:
+                text = recognizer.recognize_google(audio_data, language='es-ES')
+                print("Texto reconocido:")
+                print(text)
+                
+                if "siéntate" in text:
+                    await set_command(file_path, await predict(audio=file_path), text)
+                    return State.SIT
+                
+                elif "ven aquí" in text:
+                    await set_command(file_path, await predict(audio=file_path), text)
+                    return State.COME
+                
+                else: return None
+                
+            except sr.UnknownValueError:
+                print("Google Speech Recognition no pudo entender el audio")
+            except sr.RequestError as e:
+                print(f"No se pudo solicitar resultados de Google Speech Recognition; {e}")
+
+async def process_files():
+    archivos = os.listdir(AUDIO_FOLDER)
     for archivo in archivos:
         if archivo.lower().endswith('.mp3'):
             print(f"Nuevo archivo MP3 detectado: {archivo}")
             ruta_archivo = os.path.join(AUDIO_FOLDER, archivo)
-            
-            try: 
-                res = predict(audio=ruta_archivo)
-                if res != 4:
-                    print("Comando predicho:", res)
-                    try:
-                        response = requests.post(SERVER + '/set-command', json={'filename': archivo})
-                        if response.status_code == 200:
-                            print(f"Nombre de archivo '{archivo}' enviado al servidor correctamente.")
-                        else:
-                            print(f"Error al enviar el nombre de archivo '{archivo}' al servidor. Código de estado: {response.status_code}")                                
-                    except Exception as e:
-                        print(f"Error al enviar el nombre de archivo '{archivo}' al servidor o al eliminar el archivo: {e}")
-                else:
-                    print("Comando no reconocido.")
+            try:
+                res =  await recognize_audio_from_file(ruta_archivo)
             except Exception as e:
                 print("Error al predecir el comando de voz:", e)
-            
+            try:
+                os.remove(ruta_archivo)
+                print(f"Archivo {archivo} eliminado correctamente.")
+                return res
+            except Exception as e:
+                print(f"Error al eliminar el archivo {archivo}: {e}")
+
+    return None
+
+async def main():
+    current_state = 'STANDBY'
+    while True:
+        if current_state == 'STANDBY':
+            print("Esperando comando desde la Raspberry Pi...")
+            ret = await process_files()
+            if ret:
+                current_state = 'PROCESSING'
+            await asyncio.sleep(1)
+        elif current_state == 'PROCESSING':
+            print("Procesando comando...")
+            time.sleep(20)
+            await finish_command()
+            current_state = 'STANDBY'
+            await cleanup_files()   
+
+
+
+
+async def cleanup_files():
+    archivos = os.listdir(AUDIO_FOLDER)
+    for archivo in archivos:
+        if archivo.lower().endswith('.mp3'):
+            ruta_archivo = os.path.join(AUDIO_FOLDER, archivo)
             try:
                 os.remove(ruta_archivo)
                 print(f"Archivo {archivo} eliminado correctamente.")
             except Exception as e:
                 print(f"Error al eliminar el archivo {archivo}: {e}")
 
-    time.sleep(1)
+asyncio.run(main())
--- webServer/server.js
+++ webServer/server.js
@@ -7,68 +7,78 @@ const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 
+
 ffmpeg.setFfmpegPath(ffmpegPath);
 
+
+const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+    secureProtocol: 'TLSv1_2_method' // Especifica la versión de SSL/TLS aquí
+
+};
+
 const app = express();
-const server = http.createServer(app);
+const server = http.createServer(app, options);
 const io = new Server(server);
 
 // Middleware para el cuerpo de la solicitud
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
 const port = 3000;
 
-let command = null;
+let token = null;
+let person = null;
+let text = null;
 
-
-// Create uploads folder if it doesn't exist
+// Crea la carpeta de cargas si no existe
 const uploadsDir = path.join(__dirname, 'uploads');
 if (!fs.existsSync(uploadsDir)) {
     fs.mkdirSync(uploadsDir);
 }
 
-// Configuration for multer to store audio files
+// Configuración para multer para almacenar archivos de audio
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, uploadsDir);
     },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname));
-    }
+
 });
 
 const upload = multer({ storage: storage });
 
-// Dictionary to store usernames and associated files
+// Diccionario para almacenar tokens y archivos asociados
 let tokenFiles = {};
 
-// Socket connections handling
+// Manejo de conexiones de Socket.io
 io.on('connection', (socket) => {
     console.log('A user connected');
 
-    if (command) {
-        socket.emit('OnCommand', command);
+    // Si hay un token activo, notifica al cliente
+    if (token) {
+        socket.emit('OnCommand', {'userToken': token, person, text});
     }
 
+    // Maneja la recepción de un nuevo token
     socket.on('submit-token', (token) => {
         tokenFiles[token] = [];
         socket.emit('token-accepted');
     });
+
+    // Verifica la existencia de un token
     socket.on('check-token', (token) => {
         const exists = !!tokenFiles[token];
         socket.emit('token-exists', exists);
     });
 
+    // Maneja la desconexión de un usuario
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
-
 });
 
-// Endpoint to handle audio file uploads
+// Endpoint para manejar la subida de archivos de audio
 app.post('/upload', upload.single('audio'), (req, res) => {
     const token = req.body.token;
 
@@ -77,18 +87,18 @@ app.post('/upload', upload.single('audio'), (req, res) => {
     }
 
     if (req.file) {
-
-        const fileName = `${Date.now()}.mp3`
+        const fileName = `${Date.now()+ token}.mp3`;
         const inputFilePath = path.join(uploadsDir, req.file.filename);
         const outputFilePath = path.join(uploadsDir, fileName);
 
+        // Convierte el archivo a formato MP3
         ffmpeg(inputFilePath)
             .toFormat('mp3')
             .on('end', () => {
-                fs.unlinkSync(inputFilePath); // Remove original .webm file
+                fs.unlinkSync(inputFilePath); // Elimina el archivo original .webm
                 tokenFiles[token].push(fileName);
                 console.log(`Audio file uploaded and converted with name: ${token} , file: ${fileName}`);
-                res.status(200).send(outputFilePath); // Send the file name as response
+                res.status(200).send(outputFilePath); // Envía el nombre del archivo como respuesta
             })
             .on('error', (err) => {
                 console.error('Error converting file:', err);
@@ -100,10 +110,11 @@ app.post('/upload', upload.single('audio'), (req, res) => {
     }
 });
 
-
+// Endpoint para establecer un comando
 app.post('/set-command', (req, res) => {
-    const  filename  = req.body.filename;
+    const filename = req.body.filename;
 
+    console.log(req.body);
     let usuarioEncontrado = null;
     for (const token in tokenFiles) {
         if (tokenFiles[token].includes(filename)) {
@@ -113,29 +124,39 @@ app.post('/set-command', (req, res) => {
     }
 
     if (usuarioEncontrado) {
-        command = usuarioEncontrado;
-        io.emit('CommandAccepted', usuarioEncontrado)
+        token = usuarioEncontrado;
+        person = req.body.person;
+        text = req.body.text;
+
+        io.emit('CommandAccepted', {'userToken': usuarioEncontrado, person, text});
         res.status(200).send(`El archivo '${filename}' pertenece al usuario '${usuarioEncontrado}'.`);
     } else {
         res.status(200).send(`No se encontró ningún usuario asociado al archivo '${filename}'.`);
     }
 });
 
+// Endpoint para finalizar un comando
 app.post('/finish-command', (req, res) => {
-    command = null;
+    token = null;
+    person = null;
+    text = null;
+
     io.emit('commandFinished');
+
     res.status(200).send('Comando finalizado.');
 });
 
-// Serve static files
+// Sirve archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
 
-// Serve HTML file
+// Sirve el archivo HTML
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+
+
+// Inicia el servidor
 server.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
