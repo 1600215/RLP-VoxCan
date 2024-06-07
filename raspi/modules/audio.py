b640035e1d0b15b1ea9df5eab48d027c73258687
albertceballos0 <albertceballos37@gmail.com>
2024-06-07 13:15:21 +0200

    separación en diferentes funciones para reconocimiento de audio, ayuda a testear y a la estrcturación del proyecto, también lectura de archivos de tipo wav no de tipo mp3

--- raspi/modules/audio.py
+++ raspi/modules/audio.py
@@ -13,66 +13,92 @@ from constants import State, AUDIO_FOLDER
 from voiceIdent import predict
 
 
-async def recognize_audio_from_file(file_path):
+async def recognize_audio(file_path):
     recognizer = sr.Recognizer()
-
-    person = await predict(audio=file_path)
-    if person == 4:
-        return None
     
-    audio = AudioSegment.from_mp3(file_path)
-    wav_data = io.BytesIO()
-    audio.export(wav_data, format="wav")
-
-    with io.BytesIO(wav_data.getvalue()) as wav_io:
-        wav_io.seek(0)
-        with sr.AudioFile(wav_io) as source:
+    try:
+        # Usar speech_recognition para reconocer el audio directamente desde el archivo WAV
+        with sr.AudioFile(file_path) as source:
             audio_data = recognizer.record(source)
             try:
                 text = recognizer.recognize_google(audio_data, language='es-ES')
-                print("Texto reconocido:")
-                print(text)
-                
-                if "siéntate" in text:
-                    await set_command(file_path, await predict(audio=file_path), text)
-                    return State.SIT
-                
-                elif "ven aquí" in text:
-                    await set_command(file_path, await predict(audio=file_path), text)
-                    return State.COME
-                
-                else: return None
-                
+                return text
             except sr.UnknownValueError:
                 print("Google Speech Recognition no pudo entender el audio")
+                return None
             except sr.RequestError as e:
                 print(f"No se pudo solicitar resultados de Google Speech Recognition; {e}")
+                return None
+    except Exception as e:
+        print(f"Error al procesar el archivo de audio: {e}")
+        return None
 
 
-async def process_files():
-    archivos = os.listdir(AUDIO_FOLDER)
+async def recognize_audio_from_file(file_path):
+
+    audio = AudioSegment.from_file(file_path)
+    audio.export(file_path, format="wav")
+    
+    person = predict(audio=file_path)
+    print("Persona identificada:", person)
+    
+    if person == 4:
+        return None
+    
+    
+    text = await recognize_audio(file_path)
+    print("Texto reconocido:", text)
+    
+    if text is None:
+        return None
+    
+    if "siéntate" in text:
+        
+        res = await set_command(file_path, person, text)
+        
+        if res: return State.SIT
+        else: return None
+                
+    elif "ven aquí" in text:
+        
+        res = await set_command(file_path, person, text)
+        
+        if res: return State.COME
+        else: return None
+                
+    else: return None
+                
+
+
+async def process_files(audio_folder=AUDIO_FOLDER):
+    archivos = os.listdir(audio_folder)
+    res = None
     for archivo in archivos:
-        if archivo.lower().endswith('.mp3'):
-            print(f"Nuevo archivo MP3 detectado: {archivo}")
-            ruta_archivo = os.path.join(AUDIO_FOLDER, archivo)
+        
+        if archivo.lower().endswith('.wav'):
+            
+            print(f"Nuevo archivo WAV detectado: {archivo}")
+            ruta_archivo = os.path.join(audio_folder, archivo)
+            
             try:
-                res =  await recognize_audio_from_file(ruta_archivo)
+                res = await recognize_audio_from_file(ruta_archivo)
+                
             except Exception as e:
                 print("Error al predecir el comando de voz:", e)
+                
             try:
                 os.remove(ruta_archivo)
                 print(f"Archivo {archivo} eliminado correctamente.")
-                return res
             except Exception as e:
                 print(f"Error al eliminar el archivo {archivo}: {e}")
+    return res
 
-    return None
 
 
 async def cleanup_files():
     archivos = os.listdir(AUDIO_FOLDER)
     for archivo in archivos:
-        if archivo.lower().endswith('.mp3'):
+        if archivo.lower().endswith('.wav'):
             ruta_archivo = os.path.join(AUDIO_FOLDER, archivo)
             try:
                 os.remove(ruta_archivo)
