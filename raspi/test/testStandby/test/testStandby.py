9c19eac5cc3e6ca9ed2f6d7377488fa6d5e08681
albertceballos0 <albertceballos37@gmail.com>
2024-06-07 13:32:12 +0200

    re estructuración del test testStandby haciendolo más legible utilizando constantes y modulos

---
+++ raspi/test/testStandby/test/testStandby.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import time
+import asyncio
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+
+from modules.web import finish_command
+from modules.audio import process_files
+from constants import State
+
+AUDIO_FOLDER = 'uploads'  # Cambia esto por la ruta de tu carpeta de destino
+
+
+async def main():
+    current_state = State.STANDBY
+    while True:
+        if current_state == State.STANDBY:
+            print("Esperando comando desde la Raspberry Pi...")
+            ret = await process_files(audio_folder=AUDIO_FOLDER)
+            if ret:
+                current_state = ret
+            await asyncio.sleep(1)
+            
+        elif current_state == State.SIT:
+            print("Procesando comando SIT...")
+            time.sleep(20)
+            await finish_command()
+            current_state = State.STANDBY
+            await cleanup_files()   
+            
+        elif current_state == State.COME:
+            print("Procesando comando COME...")
+            time.sleep(20)
+            await finish_command()
+            current_state = State.STANDBY
+            await cleanup_files()
+        else: 
+            print("Estado desconocido")
+            break
+
+async def cleanup_files():
+    archivos = os.listdir(AUDIO_FOLDER)
+    for archivo in archivos:
+        ruta_archivo = os.path.join(AUDIO_FOLDER, archivo)
+        try:
+            os.remove(ruta_archivo)
+            print(f"Archivo {archivo} eliminado correctamente.")
+        except Exception as e:
+            print(f"Error al eliminar el archivo {archivo}: {e}")
+
+asyncio.run(main())
