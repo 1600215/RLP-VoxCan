49550969719f13d9e715de490fd8f5e2123969b8
albertceballos0 <albertceballos37@gmail.com>
2024-06-25 14:44:09 +0200

    testRotate

---
+++ raspi/test/testRotate180.py
@@ -0,0 +1,49 @@
+import serial,time
+import sys, os
+import asyncio
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from modules.arduino import connect
+from modules.motion import rotate_90_degrees, standup
+
+
+async def testRotate(mode="pc"):
+    if mode == "pc":
+        connection = "/dev/tty.usbserial-14130"
+    elif mode == "raspi":
+        connection = "/dev/ttyUSB0"
+    else: 
+        print("Modo no válido")
+        return False
+
+    print('Running. Press CTRL-C to exit.')
+    with serial.Serial(connection, 9600, timeout=1) as ser:
+        await asyncio.sleep(1) 
+        if ser.isOpen():
+            print("{} connected!".format(ser.port))
+            try: 
+                t0 = time.time()
+                while not connect(ser): 
+                    print("Intentando conectar...")
+                    pass
+                print("Tiempo de conexión: ", time.time()-t0)
+                
+                #task to send message by the queueAudio
+                
+                await standup(ser)
+                
+                await asyncio.sleep(5)
+                
+                # Test move_robot_with_imbalance
+                task = asyncio.create_task(rotate_90_degrees(ser))
+                await task
+                
+                print("ACABADO CAMINAR CAMBIA DE ESTADO")
+                
+            except KeyboardInterrupt:
+                print("KeyboardInterrupt has been caught.")
+                return False
+
+if __name__ == '__main__':
+    asyncio.run(testRotate())
