1ab7bcf6cd79c389ca7aff8fd0b85edd9a4ed896
albertceballos0 <albertceballos37@gmail.com>
2024-06-25 18:51:23 +0200

    feat: agregar módulo para medir distancia con sensor ultrasónico

---
+++ raspi/modules/distance.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import time, sys, os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from constants import TRIG, ECHO
+
+async def distance():
+    # Envía una señal de disparo de 10us
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
+
+    # Espera la recepción de la señal
+    while GPIO.input(ECHO) == 0:
+        pulso_inicio = time.time()
+
+    while GPIO.input(ECHO) == 1:
+        pulso_fin = time.time()
+
+    # Calcula la duración del pulso y la distancia
+    duracion_pulso = pulso_fin - pulso_inicio
+    distancia = duracion_pulso * 17150
+    distancia = round(distancia, 2)
+
+    return distancia
+
+
